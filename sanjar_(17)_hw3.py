789ba4227d5aef61c0aaa64198da6f04cf715a85
thesanjarik <sanjarbakenbaev@gmail.com>
2022-03-05 20:00:36 +0600

    Create sanjar_(17)_hw3.py
    ДЗ3

---
+++ sanjar_(17)_hw3.py
@@ -0,0 +1,44 @@
+
+
+data_tuple = ('h', 6.13, 'C', 'e', 'T', True, 'k', 'e', 3, 'e', 1, 'g')
+
+
+letters = []
+
+numbers = []
+
+for i in data_tuple:
+    if type(i) == str:
+        letters.append(i)
+    else:
+        numbers.append(i)
+
+numbers.remove(6.13)
+letters.append(numbers.pop(0))
+
+numbers.insert(1, 2)
+
+numbers.sort()
+letters.reverse()
+
+numbers[0] = numbers[0] ** 2
+numbers[1] = numbers[1] ** 2
+numbers[2] = numbers[2] ** 2
+
+letters[1] = 'G'
+letters[-2] = 'c'
+
+letters = tuple(letters)
+numbers = tuple(numbers)
+
+
+print(letters)
+print(numbers)
+
+
+
+
+
+
+
+
